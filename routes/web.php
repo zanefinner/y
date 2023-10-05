7a263c90135e323c8d4d455c0c1400ec23f8ea09
zane <zanefinnergm@gmail.com>
2023-10-05 04:00:13 -0400

    using resource with auth middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers{
+    \ProfileController,
+    \ChirpController
+};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,5 +30,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::resource('chirps', ChirpController::class)
+    ->only(['index', 'store'])
+    ->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
